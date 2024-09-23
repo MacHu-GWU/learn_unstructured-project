cd4cde2923e69d8362d12a41136b1de54d9b3fee
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2024-09-23 10:36:58 -0400

    Update test_unstructured.py

--- example/test_unstructured.py
+++ example/test_unstructured.py
@@ -3,9 +3,8 @@
 from unstructured.partition.pdf import partition_pdf
 
 elements = partition_pdf(filename="f1040.pdf")
-# print(len(elements))
+print(len(elements))
 for ith, element in enumerate(elements, start=1):
     print(f"========== Element {ith} ==========")
     print(element.to_dict())
-
-from unstructured_inference.models.base import get_model
+    print(element.metadata.coordinates)
